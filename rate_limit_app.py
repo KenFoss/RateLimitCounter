ecb76a140e4ab3222a90533d1a3e99135e88474c
Ken Foss <kenfoss0715@gmail.com>
2024-02-25 12:00:59 -0500

    require x-api-keyu

--- rate_limit_app.py
+++ rate_limit_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 from os import environ;
 
@@ -8,9 +8,18 @@ app = Flask(__name__)
 counter = 0
 last_reset_date = datetime.now().date()
 
-@app.route('/')
-def index():
+def is_valid_token(token):
+  # This method will ensure validity of token in request header
+    return token == environ.get('X_API_KEY')
+
+@app.route('/api/recipe-app/auth', methods=['GET'])
+def recipe_auth():
     global counter, last_reset_date
+
+    api_token = request.headers.get('x-api-key')
+    # If the propper key has not been passed, this request is unauthorized and should not continue
+    if not (is_valid_token(api_token)) :
+        return jsonify({"message": "Unauthorized Access"}), 401
     
     # Check if it's a new day, if so, reset the counter
     if last_reset_date != datetime.now().date():
@@ -30,4 +39,4 @@ def index():
     return jsonify({"auth_key": AUTH_KEY})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=443)
+    app.run(debug=True)
